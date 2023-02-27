a23c5b06f026cb43f6a3d26ff7088c301a758775
TatevZaqaryan <tatevzaqaryan.1998@gmail.com>
2023-02-27 15:45:38 +0400

    create logiic document body click, sort popup close+ remove add event listner page un mounte

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSort } from "../redux/slices/filterSlice";
 export const list = [
@@ -11,6 +11,7 @@ export const list = [
 ];
 
 function Sort() {
+  const sortRef = useRef();
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
   const [open, setOpen] = useState(false);
@@ -19,9 +20,19 @@ function Sort() {
     dispatch(setSort(i));
     setOpen(false);
   };
-
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+    document.body.addEventListener("click", handleClickOutside);
+    return () => {
+      document.body.remove("click", handleClickOutside);
+    };
+  }, []);
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
